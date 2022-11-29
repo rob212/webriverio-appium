0c3cb2a1b1ab09183c9939b435d22470dc9b7392
Rob McBryde <robertmcbryde83@gmail.com>
2022-11-29 12:21:47 +0000

    added confog to specify port number for appium

--- wdio.conf.js
+++ wdio.conf.js
@@ -113,7 +113,15 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['appium'],
+    services: [
+        ['appium', {
+            args: {
+                address: 'localhost',
+                port: 4723
+            },
+            logPath: './'
+        }]
+    ],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
